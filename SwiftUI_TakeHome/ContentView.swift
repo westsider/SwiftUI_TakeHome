3d51f274516ffbc5528d6c5addf586a9e2125989
westsider <whansen1@mac.com>
2021-05-16 08:34:00 -0700

    updated screen 1

--- SwiftUI_TakeHome/ContentView.swift
+++ SwiftUI_TakeHome/ContentView.swift
@@ -8,13 +8,32 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var isPresented = false
+    @State var isActive : Bool = false
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            VStack {
+                Text("Page 1")
+                Spacer()
+                        .frame(height: 120)
+                NavigationLink(destination: SecondView()) {
+                    ButtonView(message: "Go To Page 2")
+                }
+                Spacer()
+            }
+            .toolbar {
+                Button("Logout") {
+                    isPresented.toggle()
+                }
+                .fullScreenCover(isPresented: $isPresented, content: FullScreenModalView.init)
+            }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
+    @State private var isPresented = false
     static var previews: some View {
         ContentView()
     }
