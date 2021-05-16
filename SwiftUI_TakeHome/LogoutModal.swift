5aad8f087435b086fa7d5ee4c3af4588d0323a24
westsider <whansen1@mac.com>
2021-05-16 08:31:57 -0700

    added logout modal

---
+++ SwiftUI_TakeHome/LogoutModal.swift
@@ -0,0 +1,31 @@
+//
+//  LogoutModal.swift
+//  SwiftUI_TakeHome
+//
+//  Created by Warren Hansen on 5/16/21.
+//
+
+import SwiftUI
+
+struct FullScreenModalView: View {
+    @Environment(\.presentationMode) var presentationMode
+    var body: some View {
+        Spacer()
+                .frame(height: 60)
+        Text("Login Screen")
+        Spacer()
+                .frame(height: 120)
+        Button("Login") {
+            presentationMode.wrappedValue.dismiss()
+        }
+        .navigationTitle("ContentView")
+        .padding(.top, 4)
+        .padding(.bottom, 4)
+        .padding(.leading, 16)
+        .padding(.trailing, 16)
+        .foregroundColor(.white)
+        .background(Color.blue)
+        .cornerRadius(8, antialiased: true)
+        Spacer()
+    }
+}
